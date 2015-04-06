8b48ebdb92ea13cd493acd2805e14c51e9f1a8bb
JaniKibichi <janikibichi@gmail.com>
2015-04-06 18:42:15 +0300

    three

--- public/modules/customers/controllers/customers.client.controller.js
+++ public/modules/customers/controllers/customers.client.controller.js
@@ -1,12 +1,97 @@
 'use strict';
 
 // Customers controller
-angular.module('customers').controller('CustomersController', ['$scope', '$stateParams', '$location', 'Authentication', 'Customers',
-	function($scope, $stateParams, $location, Authentication, Customers) {
-		$scope.authentication = Authentication;
 
+var customersApp = angular.module('customers');
+
+customersApp.controller('CustomersController', ['$scope', '$stateParams', 'Authentication', 'Customers', '$modal', '$log',
+	function($scope, $stateParams, Authentication, Customers, $modal, $log) {
+
+				this.authentication = Authentication;
+
+				// Find a list of Customers
+				this.customers = Customers.query();
+
+				//Create
+				//Open a Modal window to create a single customer record
+				this.modalCreate = function (size) {
+
+				    var modalInstance = $modal.open({
+				      templateUrl: 'modules/customers/views/create-customer.client.view.html',
+				      controller: function ($scope, $modalInstance){
+				      	
+				      	$scope.ok = function () {
+				      		$modalInstance.close();
+						  };
+
+						  $scope.cancel = function () {
+						    $modalInstance.dismiss('cancel');
+						  };
+				      },
+				      size: size
+				    });
+
+				    modalInstance.result.then(function (selectedItem) {
+				    }, function () {
+				      $log.info('Modal dismissed at: ' + new Date());
+				    });
+				  };
+
+				//Update
+				//Open a Modal window to update a single customer record
+				this.modalUpdate = function (size, selectedCustomer) {
+
+				    var modalInstance = $modal.open({
+				      templateUrl: 'modules/customers/views/edit-customer.client.view.html',
+				      controller: function ($scope, $modalInstance, customer){
+				      	$scope.customer = customer;
+
+				      	$scope.ok = function () {
+				      			$modalInstance.close($scope.customer);
+						  };
+
+						  $scope.cancel = function () {
+						    $modalInstance.dismiss('cancel');
+						  };
+				      },
+				      size: size,
+				      resolve: {
+				        customer: function () {
+				          return selectedCustomer;
+				        }
+				      }
+				    });
+
+				    modalInstance.result.then(function (selectedItem) {
+				      $scope.selected = selectedItem;
+				    }, function () {
+				      $log.info('Modal dismissed at: ' + new Date());
+				    });
+				  };
+
+					// Remove existing Customer
+				this.remove = function(customer) {
+					if ( customer ) { 
+						customer.$remove();
+
+						for (var i in this.customers) {
+							if (this.customers [i] === customer) {
+								this.customers.splice(i, 1);
+							}
+						}
+					} else {
+						this.customer.$remove(function() {
+						});
+					}
+				};
+				
+	}
+]);
+
+customersApp.controller('CustomersCreateController', ['$scope', 'Customers','Notify',
+	function($scope, Customers, Notify) {
 		// Create new Customer
-		$scope.create = function() {
+		this.create = function() {
 			// Create new Customer object
 			var customer = new Customers ({
 				firstName: this.firstName,
@@ -20,57 +105,57 @@ angular.module('customers').controller('CustomersController', ['$scope', '$state
 				channel: this.channel
 			});
 
-			// Redirect after save
+			// Just save
 			customer.$save(function(response) {
-				$location.path('customers/' + response._id);
-
 				// Clear form fields
-				$scope.firstName = '';
-				$scope.surname = '';
-				$scope.suburb = '';
-				$scope.country = '';
-				$scope.industry = '';
-				$scope.email = '';
-				$scope.phone = '';
-				$scope.referred = '';
-				$scope.channel = '';
+				Notify.sendMsg('NewCustomer',{'id':response._id});
+
 			}, function(errorResponse) {
 				$scope.error = errorResponse.data.message;
 			});
 		};
 
-		// Remove existing Customer
-		$scope.remove = function(customer) {
-			if ( customer ) { 
-				customer.$remove();
+	}
+]);
 
-				for (var i in $scope.customers) {
-					if ($scope.customers [i] === customer) {
-						$scope.customers.splice(i, 1);
-					}
-				}
-			} else {
-				$scope.customer.$remove(function() {
-					$location.path('customers');
-				});
-			}
-		};
-
+customersApp.controller('CustomersEditController', ['$scope', 'Customers',
+	function($scope, Customers) {
 		// Update existing Customer
-		$scope.update = function() {
-			var customer = $scope.customer;
+		this.update = function(updatedCustomer) {
+			var customer = updatedCustomer;
 
 			customer.$update(function() {
-				$location.path('customers/' + customer._id);
+				
 			}, function(errorResponse) {
 				$scope.error = errorResponse.data.message;
 			});
 		};
+	}
+]);
 
-		// Find a list of Customers
-		$scope.find = function() {
-			$scope.customers = Customers.query();
-		};
+//Customer-list directive
+customersApp.directive('customerList',['Customers','Notify',function(Customers, Notify){
+	 return {
+    restrict: 'E',
+    transclude: true,
+    templateUrl: 'modules/customers/views/customer-list-template.html',
+    link: function(scope, element, attrs){
+    	//When a new customer is added update the customer list
+    	Notify.getMsg('NewCustomer',function(event, data){
+    		scope.customersCtrl.customers = Customers.query();
+    	});
+    }
+  };
+}]);
+
+		
+/*
+		
+
+		
+
+		
+		
 
 		// Find existing Customer
 		$scope.findOne = function() {
@@ -78,5 +163,4 @@ angular.module('customers').controller('CustomersController', ['$scope', '$state
 				customerId: $stateParams.customerId
 			});
 		};
-	}
-]);
+*/
--- public/modules/customers/services/customers.client.service.js
+++ public/modules/customers/services/customers.client.service.js
@@ -1,7 +1,8 @@
 'use strict';
 
 //Customers service used to communicate Customers REST endpoints
-angular.module('customers').factory('Customers', ['$resource',
+angular.module('customers')
+.factory('Customers', ['$resource',
 	function($resource) {
 		return $resource('customers/:customerId', { customerId: '@_id'
 		}, {
@@ -9,5 +10,25 @@ angular.module('customers').factory('Customers', ['$resource',
 				method: 'PUT'
 			}
 		});
+	}
+])
+
+.factory('Notify', ['$rootScope',function($rootScope) {
+	var notify = {};
+
+	notify.sendMsg = function(msg, data){
+		data = data || {};
+		$rootScope.$emit(msg, data);
+
+		console.log('message sent!');
+	};
+
+	notify.getMsg = function(msg, func, scope){
+		var unbind = $rootScope.$on(msg,func);
+		if(scope){
+			scope.$on('destroy, unbind')
+		}
+	};
+	return notify;
 	}
 ]);
